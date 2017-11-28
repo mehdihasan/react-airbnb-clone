4911ef16b77f6aa9a177357154a0c0544d87f3cb
mehdi <mehdi@cefalo.com>
2017-11-28 18:00:01 +0600

    css experiments

--- src/components/sortingbutton.js
+++ src/components/sortingbutton.js
@@ -11,13 +11,17 @@ class SortingButton extends Component {
         return (
             <div class="ms_menu_holder">
                 
-                <MsImage
-                    url="https://a0.muscache.com/ac/pictures/8b7519ec-2c82-4c09-8233-fd4d2715bbf9.jpg"
-                    height="72"
-                    width="96" />
+                <div class="ms_menu_img">
+                    <MsImage
+                        url="https://a0.muscache.com/ac/pictures/8b7519ec-2c82-4c09-8233-fd4d2715bbf9.jpg"
+                        height="72"
+                        width="96" />
+                </div>
 
-                <MsParagraph
-                    text="HOME My Home" />
+                <div class="ms_menu_text">
+                    <MsParagraph
+                        text="HOMES" />
+                </div>
                 
             </div>
         )
